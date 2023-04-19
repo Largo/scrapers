3f09f69a0eff8261246d25ffffe63dbde6520e75
Andi <andi@idogawa.dev>
2023-04-19 20:58:17 +0900

    works without

--- rent.rb
+++ rent.rb
@@ -2,13 +2,13 @@ require 'selenium-webdriver'
 require 'webdrivers'
 
 # Firefox の WebDriver インスタンスを作成する
-@driver = Selenium::WebDriver.for :firefox
+driver = Selenium::WebDriver.for :firefox
 
 # Firefox のオプションを設定する
-options = Selenium::WebDriver::Firefox::Options.new
-options.add_argument('-headless')  # ヘッドレスモードで起動する
-options.binary = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"  # Firefox の実行ファイルのパスを指定する
-driver = Selenium::WebDriver.for :firefox, options: options
+#options = Selenium::WebDriver::Firefox::Options.new
+#options.add_argument('-headless')  # ヘッドレスモードで起動する
+#options.binary = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"  # Firefox の実行ファイルのパスを指定する
+driver = Selenium::WebDriver.for :firefox#, options: options
 
 # 検索ページに移動する
 driver.navigate.to 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=090&bs=040&pc=30&smk=&po1=25&po2=99&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sc=45201&ta=45&cb=0.0&ct=10.0&et=9999999&mb=0&mt=9999999&cn=9999999&fw2='
